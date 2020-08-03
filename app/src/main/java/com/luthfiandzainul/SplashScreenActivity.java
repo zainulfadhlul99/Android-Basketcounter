1f0f24df709f4e54ac0d419c1770f80e83238dcf
unknown <zainulparji@gmail.com>
2020-08-03 19:21:07 +0700

    test

---
+++ app/src/main/java/com/luthfiandzainul/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.luthfiandzainul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.luthfiandzainul.basketcounter_eksplisit.MainActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+}
